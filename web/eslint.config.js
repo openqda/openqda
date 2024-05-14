59b49db70e569dc51a5578aee0f94c0602e05b05
jankapunkt <jkuester@uni-bremen.de>
2024-05-14 16:27:56 +0200

    build(client): add configs for code quality tools

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -2,9 +2,16 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+      languageOptions: {
+          globals: {
+              ...globals.browser,
+              Ziggy: true,
+              axios: true,
+          }
+      }
+  },
   pluginJs.configs.recommended,
   ...pluginVue.configs["flat/essential"],
 ];
