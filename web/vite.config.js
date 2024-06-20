22992f704ac4fbc5495c21dabfbe337ac1057426
jankapunkt <jkuester@uni-bremen.de>
2024-06-20 10:52:10 +0200

    refactor(client): add coverage tests

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import {defineConfig} from "vite";
 import laravel, {refreshPaths} from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
@@ -24,13 +25,31 @@ export default defineConfig({
         sourcemap: true
     },
     test: {
+        include: ['resources/js/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+        exclude: [
+            '**/node_modules/**',
+            'public/',
+            '**/dist/**',
+            '**/cypress/**',
+            '**/.{idea,git,cache,output,temp}/**',
+            'stories',
+            '.storybook',
+            'app/',
+            'bootstrap/',
+            'config/',
+            'database/',
+            'routes/',
+            '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*'],
         // enable jest-like global test APIs
         globals: true,
         // simulate DOM with happy-dom
         environment: 'happy-dom',
         coverage: {
+            enabled: true,
+            subdir: true,
+            include: ['resources/js/**/*'],
             provider: 'v8',
-            reporter: ['text', 'json', 'html']
+            reporter: ['text', 'html']
         },
     }
 });
