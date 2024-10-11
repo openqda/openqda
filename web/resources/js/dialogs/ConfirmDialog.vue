53d92f63922a2a0c0af95f100716319e851ae0e2
jankapunkt <jkuester@uni-bremen.de>
2024-10-11 12:12:50 +0200

    fix(ui): add missing COnfirmDialog and NanoHooks components

---
+++ web/resources/js/dialogs/ConfirmDialog.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import Button from '../Components/interactive/Button.vue';
+import DialogBase from './DialogBase.vue';
+import {ref, watch} from 'vue';
+
+const emit = defineEmits(['confirmed', 'cancelled']);
+const open = ref(false);
+
+const props = defineProps({
+  title: String,
+  text: String,
+});
+
+watch(
+    () => props.text,
+    () => start()
+);
+
+const start = () => {
+    open.value = true;
+};
+
+const confirm = () => {
+    open.value = false;
+    emit('confirmed');
+}
+
+const cancel = () => {
+    open.value = false;
+    emit('cancelled');
+};
+</script>
+
+<template>
+  <DialogBase :title="props.title ?? 'Confirm decision'" :show="open">
+    <template #body>
+      <p>{{ props.text }}</p>
+    </template>
+    <template #footer>
+      <div class="flex justify-between items-center w-full">
+        <Button variant="outline" @click="cancel">Cancel</Button>
+        <Button @click="confirm">Confirm</Button>
+      </div>
+    </template>
+  </DialogBase>
+</template>
+
+<style scoped></style>
