42c801a643f34a525f3021c15593a304bb2f3941
Alessandro Belli <belli@uni-bremen.de>
2025-01-29 12:52:34 +0100

    Update broadcasting.php

--- web/config/broadcasting.php
+++ web/config/broadcasting.php
@@ -39,6 +39,10 @@ return [
                 'port' => env('REVERB_PORT', 443),
                 'scheme' => env('REVERB_SCHEME', 'https'),
                 'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
+                "curl_options" => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ],
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
