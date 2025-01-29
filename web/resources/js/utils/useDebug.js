787181d6ae0ab3276d6a243df7cb5b7cf2d5d1f7
jankapunkt <jkuester@uni-bremen.de>
2025-01-29 11:11:38 +0100

    fix(ui): allow to override debug

--- web/resources/js/utils/useDebug.js
+++ web/resources/js/utils/useDebug.js
@@ -2,10 +2,13 @@ import { noop } from './function/noop.js';
 
 const DEBUG = (import.meta.env.VITE_DEBUG_CLIENT ?? import.meta.env.DEBUG_CLIENT);
 
-export const useDebug = () => {
+export const useDebug = (mode) => {
+  const type = mode ?? DEBUG;
   // eslint-disable-next-line no-console
-  const handler = console[DEBUG];
+  const handler = console[type];
   return handler
-    ? (...args) => handler.call(console, ...args)
+    ? (...args) => handler.call(console, `[${type}]:`, ...args)
     : noop;
 };
+
+window.useDebug = useDebug
