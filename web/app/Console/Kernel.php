f54b49969181551d2c528bfca6286aa99847d416
Alessandro Belli <belli@uni-bremen.de>
2025-07-03 14:28:56 +0200

    Update Kernel.php

--- web/app/Console/Kernel.php
+++ web/app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->command('queue:flush')->dailyAt('00:00');
+        $schedule->command('queue:flush')->dailyAt('00:00');
     }
 
     /**
