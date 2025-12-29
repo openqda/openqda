d1bf38773110a21391346b3108926e267f293d83
jankapunkt <jkuester@uni-bremen.de>
2025-12-29 23:01:20 +0100

    fix: rename duplicate route name

--- web/routes/web.php
+++ web/routes/web.php
@@ -72,7 +72,7 @@ Route::middleware([
 ])->group(function () {
 
     Route::post('/projects/{project}/sources/{source}/gethtmlcontent', [SourceController::class, 'retryConversion'])->name('source.convert');
-    Route::post('/projects/{project}/sources/{source}/retrytranscription', [SourceController::class, 'retryTranscription'])->name('source.convert');
+    Route::post('/projects/{project}/sources/{source}/retrytranscription', [SourceController::class, 'retryTranscription'])->name('source.transcript');
 
     /**
      * Projects
