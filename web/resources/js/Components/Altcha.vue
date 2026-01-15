3b47fd252b22c8eeecd6dce469cf4c1fa2e04a16
jankapunkt <jkuester@uni-bremen.de>
2026-01-15 11:10:36 +0100

    fix(auth): altcha updated and fixed for faulty user workflows

--- web/resources/js/Components/Altcha.vue
+++ web/resources/js/Components/Altcha.vue
@@ -1,21 +1,51 @@
-<template>
-  <altcha-widget
-    v-once
-    id="altcha_widget"
-    class="text-secondary-foreground border-0"
-    challengeurl="/altcha-challenge"
-    @statechange="stateChanged"
-    auto="off"
-  ></altcha-widget>
-</template>
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted, watch } from 'vue';
 
-<script setup>
+// Importing altcha package will introduce a new element <altcha-widget>
 import 'altcha';
 
-const emit = defineEmits(['update:modelValue']);
-const stateChanged = (ev) => {
-  if (ev.detail.state === 'verified') {
-    emit('update:modelValue', ev.detail.payload);
+const altchaWidget = ref<HTMLElement | null>(null);
+const props = defineProps({
+  payload: {
+    type: String,
+    required: false,
+  },
+});
+const emit = defineEmits(['update:payload']);
+const internalValue = ref(props.payload);
+
+watch(internalValue, (v) => {
+  emit('update:payload', v || '');
+});
+
+const onStateChange = (ev) => {
+  if ('detail' in ev) {
+    const { payload, state } = ev.detail;
+    if (state === 'verified' && payload) {
+      internalValue.value = payload;
+    } else {
+      internalValue.value = '';
+    }
   }
 };
+
+onMounted(() => {
+  if (altchaWidget.value) {
+    altchaWidget.value.addEventListener('statechange', onStateChange);
+  }
+});
+
+onUnmounted(() => {
+  if (altchaWidget.value) {
+    altchaWidget.value.removeEventListener('statechange', onStateChange);
+  }
+});
 </script>
+
+<template>
+  <!-- Configure your `challengeurl` and remove the `test` attribute, see docs: https://altcha.org/docs/v2/widget-integration/ -->
+  <altcha-widget
+    ref="altchaWidget"
+    challengeurl="/altcha-challenge"
+  ></altcha-widget>
+</template>
